29ef83b2a3c8d016b40f9b93dac4c5614dbf2e2e
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-25 13:06:38 +0200

    feat(Character): Gave Entities.Character an initial set of properties

--- Film API/Data/Entities/Character.cs
+++ Film API/Data/Entities/Character.cs
@@ -1,6 +1,20 @@
-﻿namespace Film_API.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Film_API.Data.Entities
 {
     public class Character
     {
+        public int Id {  get; set; }
+
+        [StringLength(100)]
+        public string FullName { get; set; }
+
+        [StringLength(50)]
+        public string? Alias { get; set; }
+
+        [StringLength(20)]
+        public string? Gender { get; set; }
+
+        public string? Picture { get; set; }
     }
 }
