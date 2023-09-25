0423faffd5fb714b89b654f29f86e7e6d03eb0cc
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-25 13:09:12 +0200

    feat(Character): Gave Entities.Franchise an initial set of properties

--- Film API/Data/Entities/Franchise.cs
+++ Film API/Data/Entities/Franchise.cs
@@ -1,6 +1,15 @@
-﻿namespace Film_API.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Film_API.Data.Entities
 {
     public class Franchise
     {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
     }
 }
