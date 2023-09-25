7eddcfec4f8e9c22c3ba463c9c4a8f58dc74a7f0
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-25 22:08:17 +0200

    feat(FilmDbContext): Seeded db with movies and characters, and MovieCharacter relations

--- Film API/Data/FilmDbContext.cs
+++ Film API/Data/FilmDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Film_API.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 
 namespace Film_API.Data
 {
@@ -16,6 +17,39 @@ namespace Film_API.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source = N-NO-01-01-5770\\SQLEXPRESS; Initial Catalog = Film; Integrated Security = true; Trust Server Certificate = true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Movie movie1 = new() { Id = 1, Title = "Planet of the Cakes", Genre = "Sci-fi, Cooking", ReleaseYear = 1999, Director = "Julius Cream" };
+            Movie movie2 = new() { Id = 2, Title = "King Thong", Genre = "Adventure, Adult", ReleaseYear = 2020, Director = "Fran-Sisqó Ford Copulate" };
+            Movie movie3 = new() { Id = 3, Title = "Codzilla", Genre = "Fishing, Action", ReleaseYear = 1988, Director = "Salmal Fisher" };
+
+            Character character1 = new() { Id = 1, FullName = "Ethan Food" };
+            Character character2 = new() { Id = 2, FullName = "Ace O. Bass", Alias = "Ass of Bass" };
+            Character character3 = new() { Id = 3, FullName = "Ape Lincon" };
+
+            modelBuilder.Entity<Character>().HasData(character1, character2, character3);
+            modelBuilder.Entity<Movie>().HasData(movie1, movie2, movie3);
+
+            modelBuilder.Entity<Movie>()
+            .HasMany(std => std.Characters)
+            .WithMany(sub => sub.Movies)
+            .UsingEntity<Dictionary<string, object>>(
+                "MovieCharacter",
+                l => l.HasOne<Character>().WithMany().HasForeignKey("CharacterId"),
+                r => r.HasOne<Movie>().WithMany().HasForeignKey("MovieId"),
+                je =>
+                {
+                    je.HasKey("MovieId", "CharacterId");
+                    je.HasData(
+                        new { MovieId = 1, CharacterId = 1 },
+                        new { MovieId = 1, CharacterId = 3 },
+                        new { MovieId = 2, CharacterId = 3 },
+                        new { MovieId = 3, CharacterId = 2 }
+                    );
+                }
+            );
         }
     }
 }
