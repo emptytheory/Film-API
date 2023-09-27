8714d77dfb471b6cda21c5212f976a4c02035122
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-27 14:23:24 +0200

    docs(swagger): Configured Swagger to generate documantation from xml comments

--- Film API/Program.cs
+++ Film API/Program.cs
@@ -1,4 +1,10 @@
 
+using Film_API.Data;
+using Film_API.Services.Movies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace Film_API
 {
     public class Program
@@ -7,21 +13,42 @@ namespace Film_API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Film API",
+                    Description = "API for managing a Film database",
+                    TermsOfService = new Uri("https://google.com/search?q=the+best+terms+of+service"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Michael Amundsen",
+                        Url = new Uri("https://google.com/search?q=michael+amundsen")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Made-up License",
+                        Url = new Uri("https://google.com/search?q=made-up+license")
+                    }
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+            builder.Services.AddDbContext<FilmDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Film")));
+            builder.Services.AddScoped<IMovieService, MovieService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            // if (app.Environment.IsDevelopment())
+            // {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            // }
 
             app.UseHttpsRedirection();
 
