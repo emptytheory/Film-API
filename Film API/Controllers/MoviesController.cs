f0c529ffa68e5d988518c361d7efddc01dc331eb
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-27 14:35:03 +0200

    feat(Controllers): Added MoviesController

---
+++ Film API/Controllers/MoviesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Film_API.Data;
+using Film_API.Data.Entities;
+using Film_API.Services.Movies;
+using Film_API.Data.Exceptions;
+using Film_API.Data.DTOs.Movie;
+using System.Net.Mime;
+
+namespace Film_API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        /// <summary>
+        /// Get all movies.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieBriefDTO>>> GetMovies()
+        {
+            var movieList = await _movieService.GetAllAsync();
+            // Replace with AutoMapper
+            return Ok(movieList.Select(movie => new MovieBriefDTO(movie.Id, movie.Title)));
+        }
+
+        /// <summary>
+        /// Get a movie by its id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieDetailDTO>> GetMovie(int id)
+        {
+            try
+            {
+                Movie movie = await _movieService.GetByIdAsync(id);
+
+                // Replace with AutoMapper
+                MovieDetailDTO dto = new(movie.Id, 
+                                   movie.Title, 
+                                   movie.Genre, 
+                                   movie.ReleaseYear, 
+                                   movie.Director, 
+                                   movie.Picture, 
+                                   movie.Trailer, 
+                                   movie.FranchiseId);
+
+                return Ok(dto);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+ 
+        }
+
+        // PUT: api/Movies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            //if (id != movie.Id)
+            //{
+            //    return BadRequest();
+            //}
+
+            //_context.Entry(movie).State = EntityState.Modified;
+
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!MovieExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+
+            return NoContent();
+        }
+
+        // POST: api/Movies
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+          //if (_context.Movies == null)
+          //{
+          //    return Problem("Entity set 'FilmDbContext.Movies'  is null.");
+          //}
+          //  _context.Movies.Add(movie);
+          //  await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/Movies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            //if (_context.Movies == null)
+            //{
+            //    return NotFound();
+            //}
+            //var movie = await _context.Movies.FindAsync(id);
+            //if (movie == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //_context.Movies.Remove(movie);
+            //await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return false; //(_context.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
