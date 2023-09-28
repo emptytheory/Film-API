6c341b600171601fed80cb8d5edf57d5d8041404
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-28 22:36:01 +0200

    refactor: Add profile for Movie, to change from manual to automatic mapping.

---
+++ Film API/Mappers/MovieProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Film_API.Data.DTOs.Movie;
+using Film_API.Data.Entities;
+
+namespace Film_API.Mappers
+{
+    public class MovieProfile : Profile
+    {
+        public MovieProfile()
+        {
+            CreateMap<Movie, MovieDetailDTO>()
+                .ForMember(m => m.Characters,
+                           options => options.MapFrom(m => m.Characters.Select(c => c.Id).ToArray()));
+
+            CreateMap<Movie, MovieBriefDTO>();
+
+            CreateMap<MoviePutDTO, Movie>();
+
+        }
+    }
+}
