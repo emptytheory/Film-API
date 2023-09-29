f45c48206df13ecd7a7ed5f6a3e99750c18d2f71
Michael Mørk Amundsen <michael.amundsen@no.experis.com>
2023-09-29 15:30:15 +0200

    feat(DbContext): Seed db with franchises.

--- Film API/Data/FilmDbContext.cs
+++ Film API/Data/FilmDbContext.cs
@@ -1,15 +1,10 @@
 ﻿using Film_API.Data.Entities;
 using Microsoft.EntityFrameworkCore;
-using System.IO;
 
 namespace Film_API.Data
 {
     public class FilmDbContext : DbContext
     {
-        // this is where we configure what classes in our application are going to be tables in the database. where the db is
-        // (the connection string is here)
-        // seeded data
-
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Character> Characters { get; set; }
         public DbSet<Franchise> Franchises { get; set; }
@@ -19,9 +14,12 @@ namespace Film_API.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            Movie movie1 = new() { Id = 1, Title = "Planet of the Cakes", Genre = "Sci-fi, Cooking", ReleaseYear = 1999, Director = "Julius Cream" };
-            Movie movie2 = new() { Id = 2, Title = "King Thong", Genre = "Adventure, Adult", ReleaseYear = 2020, Director = "Fran-Sisqó Ford Copulate" };
-            Movie movie3 = new() { Id = 3, Title = "Codzilla", Genre = "Fishing, Action", ReleaseYear = 1988, Director = "Salman Fisher" };
+            Franchise franchise1 = new() { Id = 1, Name = "HATE CRIME Apes", Description = "Hero Apes Try to End Crime. Criminals and minorities alike fear the 'HATE CRIME Apes'." };
+            Franchise franchise2 = new() { Id = 2, Name = "Disaster Fish" };
+
+            Movie movie1 = new() { Id = 1, Title = "Planet of the Cakes", Genre = "Sci-fi, Cooking", ReleaseYear = 1999, Director = "Julius Cream", FranchiseId = franchise1.Id};
+            Movie movie2 = new() { Id = 2, Title = "King Thong", Genre = "Adventure, Adult", ReleaseYear = 2020, Director = "Fran-Sisqó Ford Copulate", FranchiseId = franchise1.Id};
+            Movie movie3 = new() { Id = 3, Title = "Codzilla", Genre = "Fishing, Action", ReleaseYear = 1988, Director = "Salman Fisher", FranchiseId = franchise2.Id };
 
             Character character1 = new() { Id = 1, FullName = "Ethan Food" };
             Character character2 = new() { Id = 2, FullName = "Ace O. Bass", Alias = "Ass of Bass" };
@@ -29,6 +27,7 @@ namespace Film_API.Data
 
             modelBuilder.Entity<Character>().HasData(character1, character2, character3);
             modelBuilder.Entity<Movie>().HasData(movie1, movie2, movie3);
+            modelBuilder.Entity<Franchise>().HasData(franchise1, franchise2);
 
             modelBuilder.Entity<Movie>()
             .HasMany(std => std.Characters)
@@ -41,10 +40,10 @@ namespace Film_API.Data
                 {
                     je.HasKey("MovieId", "CharacterId");
                     je.HasData(
-                        new { MovieId = 1, CharacterId = 1 },
-                        new { MovieId = 1, CharacterId = 3 },
-                        new { MovieId = 2, CharacterId = 3 },
-                        new { MovieId = 3, CharacterId = 2 }
+                        new { MovieId = movie1.Id, CharacterId = character1.Id },
+                        new { MovieId = movie1.Id, CharacterId = character3.Id },
+                        new { MovieId = movie2.Id, CharacterId = character3.Id },
+                        new { MovieId = movie3.Id, CharacterId = character2.Id }
                     );
                 }
             );
